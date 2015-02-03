d11815792a117d95c0693bc6f9441b137fe71c94
Ashir Amer <ashmeer7@gmail.com>
2015-02-02 23:19:26 -0800

    Fixed some layout stuff
    Added the navigation bar images
    Fixed some navigation bugs`

---
+++ content/media/js/disable_body.js
@@ -0,0 +1,4 @@
+$('.portfolio_image').click(function() { console.log("going");$("body").css("overflow", "hidden")} );
+
+$(".modal_image_div").click(function() {$("body").css("overflow", "auto")});
+
---
+++ content/media/js/offsetPictures.js
@@ -0,0 +1,3 @@
+$(window).on("hashchange", function () {
+      window.scrollTo(window.scrollX, window.scrollY - 100);
+}) ;
